95d1b8c5da21279f89ea776addaf83d5206b9333
Hiperiosity <anastasy_ya@mail.ru>
2023-07-04 14:36:31 +0300

    удали меня

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 // CORS
 app.use(cors({
-  origin: ['http://localhost: 3000'], // потом заменить адрес на постоянный
+  origin: ['*'], // потом заменить адрес на постоянный
   credentials: true, // разрешить куки
   methods: ['GET', 'PUT', 'POST', 'PATCH', 'DEL'],
 }));
@@ -51,3 +51,4 @@ app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
+
