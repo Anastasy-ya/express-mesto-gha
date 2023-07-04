7639e69df07c958db759d4e47e7452bd11591c88
Hiperiosity <anastasy_ya@mail.ru>
2023-07-04 14:13:21 +0300

    13 пр 2 ревью

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 // const { isEmail, isURL, isStrongPassword } = require('validator');
+const { isURL } = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
