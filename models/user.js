8ede373a687cb117086ca0448c822389c453db57
Hiperiosity <anastasy_ya@mail.ru>
2023-07-04 14:11:59 +0300

    13 пр 2 ревью

--- models/user.js
+++ models/user.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
-const { isEmail, isURL, isStrongPassword } = require('validator');
+// const { isEmail, isURL, isStrongPassword } = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    // required: true,
+    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    // required: true,
+    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Сын маминой подруги',
   },
   avatar: { // Invalid avatar URL
     type: String, // mongoose.SchemaTypes.Url
-    // required: true,
+    required: true,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: [isURL, 'Invalid avatar URL'],
   },
